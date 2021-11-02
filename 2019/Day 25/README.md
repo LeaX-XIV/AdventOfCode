a0180f67143cc44f56ddc83ec7439821dd42f75b
Alessandro Borione <leax_xiv@me.com>
2021-11-02 12:13:09 +0100

    2019/25
    Add solution for part 1

---
+++ 2019/Day 25/README.md
@@ -0,0 +1,16 @@
+# Day 25-2019
+
+## Run
+To auto solve part 1 of the problem, run
+```
+$ ./script.py -auto
+```
+
+To interactively solve part 1 of the problem, run
+```
+$ ./script.py
+```
+
+## Note
+
+You will need to kill the process once started in interactive mode due to my lazyness with multithreading.
--- 2019/Day 25/script.py
+++ 2019/Day 25/script.py
@@ -2,22 +2,66 @@
 # https://adventofcode.com/2019/day/25
 from ICProgram import ICProgram as ICP
 from threading import Thread
+import sys
+
+auto = False
+commands = \
+list(reversed('''west
+take fixed point
+north
+take sand
+south
+east
+east
+north
+north
+north
+take coin
+south
+south
+west
+north
+take spool of cat6
+north
+west
+north
+'''))
+prevch = '\0'
+
 
 def listen(icp):
-	while(icp.waitForOutput()):
-		print(chr(icp.getOutput()), end='')
+    global prevch
+    while (icp.waitForOutput()):
+        ch = chr(icp.getOutput())
+        if (auto):
+            if (prevch == ' ' or prevch.isdigit()) and ch.isdecimal():
+                print(ch, end='')  # Part 1: 2181046280
+            prevch = ch
+        else:
+            print(ch, end='')
+
+
+if (len(sys.argv) > 1 and sys.argv[1] == '-auto'):
+    auto = True
 
 program = []
 with open('input.txt', 'r') as f:
-	program = f.readlines()[0].strip().split(',')
+    program = f.readlines()[0].strip().split(',')
 
 droid = ICP()
 droid.loadProgram(program)
 droid.start()
 
-Thread(target=listen, args=(droid,), daemon=True).start()
+t = Thread(target=listen, args=(droid, ), daemon=True)
+t.start()
 
-while(1):
-	for c in input():
-		droid.feedInput(ord(c))
-	droid.feedInput(10)
+if auto:
+    while len(commands) > 0:
+        ch = commands.pop()
+        droid.feedInput(ord(ch))
+    t.join()
+else:
+    while (1):
+        for c in input():
+            droid.feedInput(ord(c))
+        droid.feedInput(10)
