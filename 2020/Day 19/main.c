2c508dccdc420bd881cf0b4363dc44a0e64e5c5a
LeaX-XIV <leax_xiv@me.com>
2021-11-06 17:00:17 +0100

    2020/19
    Add solution for part 2

--- 2020/Day 19/main.c
+++ 2020/Day 19/main.c
@@ -6,6 +6,7 @@
 
 #define inputFile "input.txt"
 #define testFile1 "test1.txt"
+#define testFile2 "test2.txt"
 
 typedef struct {
 	int32 nonTerminal[2][2];
@@ -15,6 +16,7 @@ typedef struct {
 	char terminal;
 } rule;
 
+void dump_grammar(rule* grammar);
 void to_normal_form(rule* grammar);
 uint8 CYK(rule* grammar, char* string);
 
@@ -23,8 +25,11 @@ int32 part2(char** rows);
 void test(char* filename, int32 expected, int32 expected2);
 
 int main(void) {
+	setbuf(stdout, NULL);
 
-	test(testFile1, 2, 0);
+	test(testFile1, 2, 2);
+	test(testFile2, 3, 12);
+
 
 	FILE* fp;
 	char** rows = NULL;
@@ -36,10 +41,10 @@ int main(void) {
 		stb_arr_push(rows, row);
 	}
 	p1 = part1(rows);
-	// p2 = part2(rows);
+	p2 = part2(rows);
 
 	printf("Part 1: %d\n", p1);	// 107
-	// printf("Part 2: %d\n", p2);
+	printf("Part 2: %d\n", p2);	// 321
 
 	return EXIT_SUCCESS;
 }
@@ -55,16 +60,16 @@ void test(char* filename, int32 expected1, int32 expected2) {
 		stb_arr_push(rows, row);
 	}
 	p1 = part1(rows);
-	// p2 = part2(rows);
+	p2 = part2(rows);
 
 	assert(expected1 == p1);
-	// assert(expected2 == p2);
+	assert(expected2 == p2);
 }
 
 int32 part1(char** rows) {
 	rule* rules = NULL;
 	uint32 valid = 0;
-	uint32 i, line;
+	uint32 i;
 
 	for (i = 0; i < stb_arr_len(rows); ++i) {
 		char* line = rows[i];
@@ -92,17 +97,9 @@ int32 part1(char** rows) {
 		stb_arr_push(rules, r);
 	}
 
-	line = i + 1;
-
 	to_normal_form(rules);
-	qsort(rules, stb_arr_len(rules), sizeof(rule), stb_intcmp((int)(&((rule*)(0x0))->pos)));
-
-	// for (i = 0; i < stb_arr_len(rules); ++i) {
-	// 	rule r = rules[i];
-	// 	printf("pos: %3d isTerminal: %1d, terminal: %d, nonTerminal: %d %d %d %d\n", r.pos, r.isTerminal, r.terminal, r.nonTerminal[0][0], r.nonTerminal[0][1], r.nonTerminal[1][0], r.nonTerminal[1][1]);
-	// }
-
-	for (i = line; i < stb_arr_len(rows); ++i) {
+	// dump_grammar(rules);
+	for (++i; i < stb_arr_len(rows); ++i) {
 		valid += CYK(rules, rows[i]);
 	}
 
@@ -110,7 +107,87 @@ int32 part1(char** rows) {
 }
 
 int32 part2(char** rows) {
-	return 0;
+	rule* rules = NULL;
+	uint32 valid = 0;
+	int32 i, line_n;
+
+	for (i = 0; i < stb_arr_len(rows); ++i) {
+		char* line = rows[i];
+		if (strlen(line) == 0) {
+			break;
+		}
+
+		rule r = { .pos = 0, .isTerminal = FALSE, .terminal = 0, .nonTerminal = { {-1, -1}, {-1, -1}}, .isStart = FALSE };
+
+		if (sscanf(line, "%d: %d %d | %d %d", &r.pos, &r.nonTerminal[0][0], &r.nonTerminal[0][1], &r.nonTerminal[1][0], &r.nonTerminal[1][1]) == 5 ||
+			sscanf(line, "%d: %d %d", &r.pos, &r.nonTerminal[0][0], &r.nonTerminal[0][1]) == 3 ||
+			sscanf(line, "%d: %d | %d", &r.pos, &r.nonTerminal[0][0], &r.nonTerminal[1][0]) == 3 ||
+			sscanf(line, "%d: %d", &r.pos, &r.nonTerminal[0][0]) == 2) {
+			if (r.pos == 0) {
+				r.isStart = TRUE;
+			}
+		}
+		else if (sscanf(line, "%d: \"%c\"", &r.pos, &r.terminal) == 2) {
+			r.isTerminal = TRUE;
+		}
+		else {
+			assert(FALSE);
+		}
+
+		stb_arr_push(rules, r);
+	}
+
+	line_n = i + 1;
+
+	for (--i; i >= 0; --i) {
+		rule r = rules[i];
+		if (r.pos == 8) {
+			rule newRule8 = {
+				.pos = r.pos,
+				.isStart = r.isStart,
+				.isTerminal = r.isTerminal,
+				.nonTerminal = {{42, -1}, {42, 8}},
+				.terminal = r.terminal
+			};
+			rules[i] = newRule8;
+		}
+		else if (r.pos == 11) {
+			rule newRule11 = {
+				.pos = r.pos,
+				.isStart = r.isStart,
+				.isTerminal = r.isTerminal,
+				.nonTerminal = {{42, 31}, {42, line_n}},
+				.terminal = r.terminal
+			};
+			rules[i] = newRule11;
+
+			rule newRule = {
+				.pos = line_n,
+				.isStart = FALSE,
+				.isTerminal = FALSE,
+				.nonTerminal = {{11, 31}, {-1, -1}},
+				.terminal = 0
+			};
+			stb_arr_push(rules, newRule);
+		}
+	}
+
+
+	to_normal_form(rules);
+	// dump_grammar(rules);
+	for (i = line_n; i < stb_arr_len(rows); ++i) {
+		valid += CYK(rules, rows[i]);
+	}
+
+	return valid;
+}
+
+void dump_grammar(rule* grammar) {
+	uint32 i;
+	for (i = 0; i < stb_arr_len(grammar); ++i) {
+		rule r = grammar[i];
+		printf("pos: %3d, isStart: %d, isTerminal: %1d, terminal: %d, nonTerminal: %d %d %d %d\n", r.pos, r.isStart, r.isTerminal, r.terminal, r.nonTerminal[0][0], r.nonTerminal[0][1], r.nonTerminal[1][0], r.nonTerminal[1][1]);
+	}
 }
 
 // https://en.m.wikipedia.org/wiki/Chomsky_normal_form#Converting_a_grammar_to_Chomsky_normal_form
@@ -127,10 +204,8 @@ void to_normal_form(rule* grammar) {
 			rule newStart = { .pos = n, .nonTerminal = {{r.pos, -1}, {-1, -1}}, .isTerminal = FALSE, .terminal = 0, .isStart = TRUE };
 			stb_arr_push(grammar, newStart);
 			++n;
-			// printf("Starting terminal changed from %d to %d\n", r.pos, newStart.pos);
 		}
 	}
-
 
 	// TERM: Eliminate rules with nonsolitary terminals
 	//  Can assume input has no rules with non solitary terminals
@@ -144,7 +219,6 @@ void to_normal_form(rule* grammar) {
 	// DEL: Eliminate ε-rules
 	//  Can assume input has no empty symbol
 	//  Skipped
-	// printf("DEL\n");
 
 	// UNIT: Eliminate unit rules
 	for (i = n-1; i >= 0; --i) {
@@ -152,14 +226,10 @@ void to_normal_form(rule* grammar) {
 		if (!r.isTerminal) {
 			uint32 b = r.nonTerminal[0][0];
 			uint32 c = r.nonTerminal[0][1];
-			uint32 d = r.nonTerminal[1][0];
-			uint32 e = r.nonTerminal[1][1];
-			if ((b != -1 && c == -1) || (d != -1 && e == -1)) {
-				// printf("Detected unit rule %d\n", r.pos);
+			if (b != -1 && c == -1) {
 				for (j = 0; j < n; ++j) {
 					rule r2 = grammar[j];
-					if (!r2.isStart && r2.pos == b || r2.pos == d) {
-						// printf("  Changing rule %d\n", r2.pos);
+					if (!r2.isStart && r2.pos == b) {
 						rule newRule = {
 							.pos = r.pos,
 							.isStart = r2.isStart,
@@ -171,8 +241,26 @@ void to_normal_form(rule* grammar) {
 
 					}
 				}
-				stb_arr_deleten(grammar, i, 1);
-				--n;
+				r.nonTerminal[0][0] = -1;
+			}
+			uint32 d = r.nonTerminal[1][0];
+			uint32 e = r.nonTerminal[1][1];
+			if (d != -1 && e == -1) {
+				for (j = 0; j < n; ++j) {
+					rule r2 = grammar[j];
+					if (!r2.isStart && r2.pos == d) {
+						rule newRule = {
+							.pos = r.pos,
+							.isStart = r2.isStart,
+							.isTerminal = r2.isTerminal,
+							.nonTerminal = {{r2.nonTerminal[0][0], r2.nonTerminal[0][1]}, {r2.nonTerminal[1][0], r2.nonTerminal[1][1]}},
+							.terminal = r2.terminal };
+						stb_arr_push(grammar, newRule);
+						++n;
+
+					}
+				}
+				r.nonTerminal[1][0] = -1;
 			}
 		}
 	}
@@ -189,7 +277,6 @@ uint8 CYK(rule* grammar, char* string) {
 
 	int l, s, p, v;
 
-	// printf("%s\n", string);
 
 	if (n == 0) return 0;
 
@@ -206,12 +293,10 @@ uint8 CYK(rule* grammar, char* string) {
 		for (v = 0; v < r; ++v) {
 			rule rule = grammar[v];
 			if (rule.isTerminal && rule.terminal == string[l]) {
-				// printf("  Matched terminal rule %d: -> '%c'\n", rule.pos, rule.terminal);
 				P[0][l][rule.pos] = TRUE;
 			}
 		}
 	}
-
 
 	// Match non terminal rules
 	for (l = 1; l < n; ++l) { // Length of substring considered
@@ -225,14 +310,13 @@ uint8 CYK(rule* grammar, char* string) {
 						uint32 d = rule.nonTerminal[1][0];
 						uint32 e = rule.nonTerminal[1][1];
 
-						if (P[p][s][b] && P[l-p-1][s+p+1][c]) {
-							// printf("    Matched non terminal rule %d -> %d %d\n", rule.pos, b, c);
-							P[l][s][rule.pos] = TRUE;
+						if (b != -1 && c != -1) {
+							if (P[p][s][b] && P[l-p-1][s+p+1][c]) {
+								P[l][s][rule.pos] = TRUE;
+							}
 						}
-
 						if (d != -1 && e != -1) {
 							if (P[p][s][d] && P[l-p-1][s+p+1][e]) {
-								// printf("    Matched non terminal rule %d -> %d %d\n", rule.pos, d, e);
 								P[l][s][rule.pos] = TRUE;
 							}
 						}
@@ -241,26 +325,6 @@ uint8 CYK(rule* grammar, char* string) {
 			}
 		}
 	}
-
-	// for (l = 0; l < n; ++l) {
-	// 	printf("P[%d]:   ", l);
-	// 	for (s = 0; s < n; ++s) {
-	// 		for (v = 0; v < r; ++v) {
-	// 			if (P[l][s][v]) {
-	// 				// printf("P[%d][%d][%d] = %d", l, s, v, v);
-	// 				printf("%d", grammar[v].pos);
-	// 			}
-	// 			// else {
-	// 			// 	printf(" ");
-	// 			// }
-	// 		}
-	// 		printf(" ");
-	// 	}
-	// 	printf("\n");
-	// }
-
-
-	// printf("Is Valid: %d  ---------------------\n", P[n-1][0][0]);
 
 	return P[n-1][0][0];
 }
